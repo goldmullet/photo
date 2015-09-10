9d5242ff773f68fec26a759777ea131dbb8a562c
Chip Neff <chiza007@yahoo.com>
2015-09-10 14:58:15 -0700

    removed post/comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910190254) do
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20150910185237) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
